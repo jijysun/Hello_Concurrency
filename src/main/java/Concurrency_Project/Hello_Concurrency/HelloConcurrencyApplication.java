b2b0f3bbbc5ca41834d86e8aef0865022399c244
jijysun <jijysun@gmail.com>
2025-05-07 15:38:46 +0900

    create project

---
+++ src/main/java/Concurrency_Project/Hello_Concurrency/HelloConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package Concurrency_Project.Hello_Concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloConcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Concurrency_Project/Hello_Concurrency/HelloConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package Concurrency_Project.Hello_Concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
